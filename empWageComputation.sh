ec44758c85c1a89c52b12ae0dd2c5de670852828
VaishnaviPawar1297 <vaishnavip1012@gmail.com>
2020-08-05 20:38:26 +0530

    Added UC2

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,3 +10,15 @@ else
 	employ="Absent"
 	echo $employ
 fi
+#--------------------------------------------------------
+if [ "$employ" == "Present" ]
+then
+	Employwageperhour=20
+	worktime=8
+	dailyemploywage=$(($Employwageperhour * $worktime))
+	echo $dailyemploywage
+else
+	dailyemploywage=0
+	echo $dailyemploywage
+fi
+
