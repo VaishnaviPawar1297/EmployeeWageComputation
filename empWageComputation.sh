5b052d0a1e1e29c40fb028f1ad338e8260dd2168
VaishnaviPawar1297 <vaishnavip1012@gmail.com>
2020-08-05 20:00:21 +0530

    Added Uc1

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation"
+#
+Attendence=$(( $RANDOM % 2 + 1 ))
+if [ $Attendence -eq 1 ]
+then
+	employ="Present"
+	echo $employ
+else
+	employ="Absent"
+	echo $employ
+fi
